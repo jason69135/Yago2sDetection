c4a74cd50397d115c326b7c9a54673511ef307c7
yangfan <yangfan@172.30.54.159>
2014-08-05 13:08:11 +0800

    a

--- Yago2sDetection/src/core/readCsv.java
+++ Yago2sDetection/src/core/readCsv.java
@@ -9,7 +9,7 @@ import java.util.List;
 import DBUtils.PostgresDBUtils;
 
 /**
- * java读取csv文件
+ * java
  * 
  * @title: readCsv
  */
@@ -53,12 +53,12 @@ public class readCsv {
 				}
 			}
 		} catch (SQLException e) {
-			System.out.println("获取数据出错");
+			System.out.println("Data Error");
 			e.printStackTrace();
 		} finally {
 			dbu.close(conn, st1, rs);
 		}
 		long endTime=System.currentTimeMillis(); 
-		System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
+		System.out.println("Time:"+(endTime-startTime)+"ms");
 	}
 }
